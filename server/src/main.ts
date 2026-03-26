8c21597887f361b351df296ca719f9f9d4df7a0e
BakingPancakes <zmorales@umass.edu>
2026-03-26 01:10:15 -0400

    calls to /articles now returns mock data when a preview is requested, otherwise error

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,6 +12,27 @@ app.get("/api", (_req, res) => {
   res.status(200).json({ message: "If you see this, the server is up!" });
 });
 
+app.get("/api/articles", (req, res) => {
+  try {
+    const requestType = req.query.requestType;
+    // const pageName = req.query.pageName;
+    if (requestType === "preview") {
+      // make database request with pageName
+      res.status(200).json({
+        title: "Test Title.",
+        thumbnail: "Fake picture",
+        summary: "blah blah blah",
+      });
+    } else {
+      res.status(400).json({
+        error: "Invalid request type. Valid request types: preview",
+      });
+    }
+  } catch (error) {
+    console.log("Error retrieving article information:", error);
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Starting ExpressJS server on ${PORT}...`);
+  console.log(`Starting ExpressJS server on port ${PORT}...`);
 });
